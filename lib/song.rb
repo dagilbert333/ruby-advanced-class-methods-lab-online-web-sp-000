52246888804e8a0b0a70882da9583a194627aa51
David Gilbert <dagilbert333@gmail.com>
2019-02-15 05:23:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,7 +5,49 @@ class Song
   def self.all
     @@all
   end
+  
+  def self.create
+    song = self.new 
+    @@all << song
+    song
+  end
+  
+  def self.new_by_name(string_name)
+    song = self.new
+    song.name = string_name
+    song 
+  end
+  
+  def self.create_by_name(string_name)
+    song = self.new
+    song.name = string_name
+    song.save
+    song
+  end
+  
+  def self.find_by_name(string_name)
+    @@all.find{|song| song.name == string_name}
+  end
 
+  def self.find_or_create_by_name(search_this_song)
+    found_song = self.find_by_name(search_this_song)
+      if found_song == nil 
+         self.create_by_name(search_this_song)
+      else
+         found_song
+      end
+  end
+  
+  def self.alphabetical
+    @@all.sort_by! {|a| a.name }
+  end
+  
+  def self.new_from_filename(filename)
+    artist_and_song = filename.split(" - ")
+    artist_and_song[1].slice! ".mp3"
+    artist_and_song
+  end
+  
   def save
     self.class.all << self
   end
